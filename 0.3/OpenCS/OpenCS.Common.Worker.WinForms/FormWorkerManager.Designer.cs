1e60a5500d3100eda4de67018a8cc59c78013e69
yeoupooh <yeoupooh@38816760-f555-0410-bd38-8322739caae8>
2008-09-04 15:29:06 +0000

    * added worker implementatinos for winforms.

---
+++ 0.3/OpenCS/OpenCS.Common.Worker.WinForms/FormWorkerManager.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace OpenCS.Common.Worker.WinForms
+{
+    partial class FormWorkerManager
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Loading...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 59);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(560, 13);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // FormWorkerManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 84);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "FormWorkerManager";
+            this.Text = "$[LABEL_IN_PROGRESS]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ 0.3/OpenCS/OpenCS.Common/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Threading;
+
+namespace OpenCS.Common
+{
+    /// <summary>
+    /// A Generic Singleton Pattern in C#
+    /// ref: http://sanity-free.org/132/generic_singleton_pattern_in_csharp.html
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class Singleton<T> where T : new()
+    {
+        static Mutex mutex = new Mutex();
+        static T instance;
+
+        /// <summary>
+        /// Get Singleton Class Instance
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                mutex.WaitOne();
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+                mutex.ReleaseMutex();
+                return instance;
+            }
+        }
+    }
+}
