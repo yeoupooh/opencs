a67f7e52732fab5bc109893d85c99bb30e4bd971
yeoupooh <yeoupooh@38816760-f555-0410-bd38-8322739caae8>
2008-09-09 09:31:27 +0000

    * added demo app and demopluginhost.

---
+++ 0.3/OpenCS/DemoApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenCS.Common.Plugin;
+using System.IO;
+
+namespace DemoApp
+{
+    public partial class Form1 : Form
+    {
+        private IPluginHost mPluginHost = new DemoPluginHost();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mPluginHost.ExecutingPath = Path.GetFullPath(".");
+            mPluginHost.LoadPlugins(Path.GetFullPath(textBoxPluginFolder.Text));
+        }
+    }
+}
