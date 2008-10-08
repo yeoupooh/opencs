ffd769c1cdefa53890666e31210ac271d3085795
yeoupooh <yeoupooh@38816760-f555-0410-bd38-8322739caae8>
2008-10-08 07:23:06 +0000

    * added OpenCS.Common.Logging.WinForms that implement OpenCS.Common.Logging.  * released 0.3.0.1.

---
+++ trunk/0.3/OpenCS/OpenCS.Common.Logging.WinForms/ListBoxLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenCS.Common.Logging;
+using System.Windows.Forms;
+
+namespace OpenCS.Common.Logging.WinForms
+{
+    public class ListBoxLogger : AbstractLogger
+    {
+        private ListBox m_listBox;
+
+        public ListBoxLogger(ListBox listBox)
+            : base(true)
+        {
+            m_listBox = listBox;
+        }
+
+        protected override void Log(string prefix, string msg)
+        {
+            m_listBox.Items.Add("[" + prefix + "] " + msg);
+            m_listBox.SelectedIndex = m_listBox.Items.Count - 1;
+        }
+    }
+}
--- trunk/0.3/OpenCS/OpenCS.Common.Logging.WinForms/Properties/AssemblyInfo.cs
+++ trunk/0.3/OpenCS/OpenCS.Common.Logging.WinForms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리의 일반 정보는 다음 특성 집합을 통해 제어됩니다.
 // 어셈블리와 관련된 정보를 수정하려면
 // 이 특성 값을 변경하십시오.
-[assembly: AssemblyTitle("OpenCS.Common.AutoUpdate")]
+[assembly: AssemblyTitle("OpenCS.Common.Logging.WinForms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenCS.Common.AutoUpdate")]
+[assembly: AssemblyProduct("OpenCS.Common.Logging.WinForms")]
 [assembly: AssemblyCopyright("Copyright (C)  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("e983b64c-c649-4818-95ba-a9f7bf35848e")]
+[assembly: Guid("d6db5e88-d214-4e3c-aa20-717b035645a0")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
